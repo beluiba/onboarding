be7b279eef6f7265af758bc7cd25466833fa827f
beluiba <belenibanezm@gmail.com>
2016-10-19 02:29:10 -0300

    new great bundle ;)

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+
+gulp.task('styles', function() {
+    return sass('css/scss/main.sass', { style: 'expanded' })
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest('css/'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(cssnano())
+        .pipe(gulp.dest('css/'))
+        .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('css/scss/*.sass', ['styles']);
+
+});
